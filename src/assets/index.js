4f1458629a358a12b8a5e0cb6e3c90f94a0c46f8
Josef Bnm <ssefbnm@gmail.com>
2023-08-06 01:46:12 +0100

    initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import { ReactComponent as Logo } from './logo.svg'
+import { ReactComponent as IconGithub } from './Github.svg'
+import { ReactComponent as IconLinkedin } from './linkedin.svg'
+import { ReactComponent as IconTwitter } from './twitter.svg'
+import { ReactComponent as IconFacebook } from './facebook.svg'
+ 
+export {
+    Logo,
+    IconGithub,
+    IconLinkedin,
+    IconTwitter,
+    IconFacebook,
+}
---
+++ src/constants/index.js
@@ -0,0 +1,47 @@
+import { IconFacebook, IconGithub, IconLinkedin, IconTwitter } from "../assets"
+
+export const navLinks = [
+    {
+        id: 'home',
+        title: 'Home'
+    },
+    {
+        id: 'projects',
+        title: 'Projects'
+    },
+    {
+        id: 'stack',
+        title: 'Stack'
+    },
+    {
+        id: 'contact',
+        title: 'Contact'
+    }
+]
+
+export const heroContent = {
+    heading: "hi I'm youcef bnm",
+    paragraph: "Bringing pixels to life, Crafting digital \N experience with frontend magic.",
+    socialLinks: [
+        {
+            id: 'social-link-github',
+            icon: IconGithub,
+            link: ''
+        },
+        {
+            id: 'social-link-linkedin',
+            icon: IconLinkedin,
+            link: ''
+        },
+        {
+            id: 'social-link-twitter',
+            icon: IconTwitter,
+            link: ''
+        },
+        {
+            id: 'social-link-facebook',
+            icon: IconFacebook,
+            link: ''
+        }
+    ]
+}
